8945d8d34ddef04838674032665034f1261d1545
madImpulse <gurke2@gmx.ch>
2025-03-17 17:52:14 +0100

    enter and no gui for gaming

--- keyboards/splitkb/elora/rev1/keymaps/madkeys/keymap.c
+++ keyboards/splitkb/elora/rev1/keymaps/madkeys/keymap.c
@@ -96,8 +96,8 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
       KC_ESC  , _______,KC_1 ,  KC_2   ,  KC_3  ,   KC_4 ,         KC_LSFT,     KC_RSFT,          KC_6 ,  KC_7 ,  KC_8 ,   KC_9 ,  KC_0 , KC_ESC,
       KC_TAB  , _______,KC_Q ,  KC_W   ,  KC_E  ,   KC_R ,         KC_LCTL,     KC_RCTL,          KC_Y ,  KC_U ,  KC_I ,   KC_O ,  KC_P , KC_BSPC,
       KC_LSFT , KC_LSFT,KC_A ,  KC_S   ,  KC_D  ,   KC_F ,         KC_LALT,     KC_RALT,          KC_H ,  KC_J ,  KC_K ,   KC_L ,KC_SCLN, KC_QUOT,
-      KC_LCTL , KC_LCTL,KC_Z ,  KC_X   ,  KC_C  ,   KC_V , KC_5  , KC_G   ,     FKEYS  , _______, KC_N ,  KC_M ,KC_COMM, KC_DOT ,KC_SLSH, KC_RSFT,
-                                KC_ENT ,    KC_T, KC_SPC, KC_SPC, KC_SPC ,      KC_SPC    , KC_SPC ,KC_RALT, KC_RGUI, ADJUST,
+      KC_LCTL , KC_LCTL,KC_Z ,  KC_X   ,  KC_C  ,   KC_V , KC_5  , KC_G   ,     MS_BTN1, KC_RGUI, KC_N ,  KC_M ,KC_COMM, KC_DOT ,KC_SLSH, KC_RSFT,
+                                KC_ENT ,    KC_T, KC_SPC, KC_SPC, KC_SPC ,      KC_SPC    , KC_SPC ,KC_ENT, KC_RCTL, ADJUST,
 
       KC_MPRV, KC_MNXT, KC_MPLY, KC_MSTP,    KC_MUTE,                            KC_MPRV, KC_MNXT, KC_MPLY, KC_MSTP,    KC_MUTE
     ),
