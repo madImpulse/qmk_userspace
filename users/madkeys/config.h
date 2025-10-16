462d8ae40191854a1b75dced1def173facb17467
madImpulse <gurke2@gmx.ch>
2025-10-16 21:55:01 +0200

    adjust timing

--- users/madkeys/config.h
+++ users/madkeys/config.h
@@ -3,9 +3,9 @@
 /* see also https://docs.qmk.fm/tap_hold */
 #define PERMISSIVE_HOLD
 /* #define HOLD_ON_OTHER_KEY_PRESS */
-#define TAPPING_TERM 160
+#define TAPPING_TERM 180
 
-#define FLOW_TAP_TERM 120
+#define FLOW_TAP_TERM 50
 
 #define BOTH_SHIFTS_TURNS_ON_CAPS_WORD
 
