5e6e5757f98ffa28153b5b613d0f42188ba2c7f7
madImpulse <gurke2@gmx.ch>
2026-01-12 21:01:23 +0100

    harmonize position of alt and enter

--- keyboards/splitkb/elora/rev1/keymaps/madkeys/keymap.c
+++ keyboards/splitkb/elora/rev1/keymaps/madkeys/keymap.c
@@ -67,7 +67,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
       KC_TAB  , MADROWQWERT,                                        KC_LCTL,     KC_RCTL,             MADROWYUIOP,                            MADDEL,
       MADCTL  , MADROWASDFG,                                        KC_LALT,     KC_RALT,             MADROWHJKL, MADROWSEMICOL,              MADQUOT,
       KC_LSFT , MADROWZXCVB,                               KC_LBRC, CW_TOGG,     MS_BTN1, KC_RBRC,    MADROWNMCDS,                            KC_RSFT,
-                             ADJUST , KC_LGUI, MADENTRSFT, RAISESP, LOWSP  ,     LOWSP  , RAISESP,    MADRALTENT, KC_RGUI, ADJUST,
+                             ADJUST , KC_LGUI, MADENTRSFT, RAISESP, LOWSP  ,     LOWSP  , RAISESP,    KC_RALT, KC_ENT, KC_RCTL,
 
       KC_MPRV, KC_MNXT, KC_MPLY, KC_MSTP,    KC_MUTE,                            KC_MPRV, KC_MNXT, KC_MPLY, KC_MSTP,    KC_MUTE
     ),
@@ -92,11 +92,11 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  * `----------------------------'      `------'                 `----------------------------'      '------'
  */
     [_GWSAD] = LAYOUT_elora_grid_wrapper(
-      KC_ESC  , _______,KC_1 ,  KC_2   ,  KC_3  ,   KC_4 ,         KC_LSFT,     KC_RSFT,          KC_6 ,  KC_7 ,  KC_8 ,   KC_9 ,  KC_0 , KC_ESC,
-      KC_TAB  , _______,KC_Q ,  KC_W   ,  KC_E  ,   KC_R ,         KC_LCTL,     KC_RCTL,          KC_Y ,  KC_U ,  KC_I ,   KC_O ,  KC_P , KC_BSPC,
-      KC_LSFT , KC_LSFT,KC_A ,  KC_S   ,  KC_D  ,   KC_F ,         KC_LALT,     KC_RALT,          KC_H ,  KC_J ,  KC_K ,   KC_L ,KC_SCLN, KC_QUOT,
+      KC_ESC  , _______,KC_1 ,  KC_2   ,  KC_3  ,   KC_4 ,         KC_LSFT,     KC_RSFT,          KC_6 ,  KC_7 ,  KC_8 ,   KC_9 ,  KC_0 , KC_BSPC,
+      KC_TAB  , _______,KC_Q ,  KC_W   ,  KC_E  ,   KC_R ,         KC_LCTL,     KC_RCTL,          KC_Y ,  KC_U ,  KC_I ,   KC_O ,  KC_P , KC_DEL,
+      KC_LSFT , KC_LSFT,KC_A ,  KC_S   ,  KC_D  ,   KC_F ,         KC_LALT,     KC_RALT,          KC_H ,  KC_J ,  KC_K ,   KC_L ,MADROWSEMICOL, KC_QUOT,
       KC_LCTL , KC_LCTL,KC_Z ,  KC_X   ,  KC_C  ,   KC_V , KC_5  , KC_G   ,     MS_BTN1, KC_RGUI, KC_N ,  KC_M ,KC_COMM, KC_DOT ,KC_SLSH, KC_RSFT,
-                                KC_ENT ,    KC_T, KC_SPC, KC_SPC, KC_SPC ,      KC_SPC    , KC_SPC ,KC_ENT, KC_RCTL, ADJUST,
+                                ADJUST ,    KC_T, KC_SPC, KC_SPC, KC_SPC ,      KC_SPC    , KC_SPC, KC_RALT, KC_ENT, KC_RCTL,
 
       KC_MPRV, KC_MNXT, KC_MPLY, KC_MSTP,    KC_MUTE,                            KC_MPRV, KC_MNXT, KC_MPLY, KC_MSTP,    KC_MUTE
     ),
